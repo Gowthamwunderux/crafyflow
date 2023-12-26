a5af3eb9a67329f55172f848e336fab50e7b19ac
NagarajWunderux <nagarajt@wunderux.com>
2023-12-26 15:15:06 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import MiniDrawer from './Components/Main/SideBar/SideBar';
 import SignUp from './Components/SignUp/SignUp';
 import { ThemeProvider,createTheme } from '@mui/material';
 function App() {
-
+const theme=createTheme({
+  
+})
 
 
   return (
