df9a0d558c45dc37308c57238395257ab57f5e09
NagarajWunderux <nagarajt@wunderux.com>
2023-12-26 16:48:14 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Drag from './Components/Main/DragAndDrop/Drag';
 import Main from './Components/Main/Main';
 import MiniDrawer from './Components/Main/SideBar/SideBar';
 import SignUp from './Components/SignUp/SignUp';
-import { ThemeProvider, createTheme } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 function App() {
-  const theme = createTheme({});
-
+  const theme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
   return (
-    <div className="App">
-      <h1>hello world </h1>
-      {/* <SignUp/> */}
-      <Main />
-      <MiniDrawer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <h1>hello world </h1>
+        {/* <SignUp/> */}
+        <Main />
+        <MiniDrawer />
+      </div>
+    </ThemeProvider>
   );
 }
 export default App;
--- src/Components/Main/Theme.jsx
+++ src/Components/Main/Theme.jsx
@@ -1,11 +1,1 @@
 // theme.js
-import { createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-  palette: {
-    mode: 'dark', // Set the mode to dark
-    // Add any other dark mode specific configurations here
-  },
-});
-
-export default theme;
