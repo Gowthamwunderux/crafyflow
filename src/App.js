b15ed9311e2080395dcfbec1d7c3507a638a829f
NagarajWunderux <nagarajt@wunderux.com>
2023-12-26 16:13:10 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
-
 import './App.css';
 import Drag from './Components/Main/DragAndDrop/Drag';
 import Main from './Components/Main/Main';
 import MiniDrawer from './Components/Main/SideBar/SideBar';
 import SignUp from './Components/SignUp/SignUp';
-import { ThemeProvider,createTheme } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material';
 function App() {
-const theme=createTheme({
-  
-})
-
+  const theme = createTheme({});
 
   return (
-   
     <div className="App">
       <h1>hello world </h1>
       {/* <SignUp/> */}
-    
       <Main />
-    <MiniDrawer />
-
- 
+      <MiniDrawer />
     </div>
- 
   );
 }
 export default App;
-
-  
